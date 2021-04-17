946c1837bc4f4709e6bf63a5cd415f341bfab3b0
Eric Ong <ako.si.eric@gmail.com>
2021-04-18 01:57:36 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+motobit.enable(MotorPower.On)
+basic.showString("btn")
+basic.pause(1000)
+motobit.enable(MotorPower.Off)
+basic.clearScreen()
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showIcon(IconNames.Yes)
+        motobit.enable(MotorPower.On)
+        basic.pause(1000)
+        motobit.enable(MotorPower.Off)
+        basic.clearScreen()
+    }
+})
