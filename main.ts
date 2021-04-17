eb1a07ce6fd44e5b9b724a1c4d9fa381d9302242
Eric Ong <ako.si.eric@gmail.com>
2021-04-18 02:46:23 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ motobit.enable(MotorPower.Off)
 basic.clearScreen()
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        basic.showIcon(IconNames.Yes)
         motobit.enable(MotorPower.On)
-        basic.pause(1000)
+        basic.showIcon(IconNames.Yes)
+        basic.pause(200)
         motobit.enable(MotorPower.Off)
         basic.clearScreen()
     }
